3065706954f14551913b12c8d310acf63da3f428
CLAUDIU-MIHAEL TERENCHE <135633780+Claudiu-Mihael-Terenche@users.noreply.github.com>
2024-04-26 13:00:39 -0400

    Delete Client directory

--- Client/src/utils/newRequest.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api/", // DEV
-  //baseURL: "/api/", // PROD
-  withCredentials: true,
-});
-
-export default newRequest;
